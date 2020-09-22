eeb928ca5d64128a6c8adcff1d3552ebfab0e01a
Mark Delk <jethrodaniel@gmail.com>
2020-09-21 19:59:48 -0500

    mv bin/ code into class method

--- lib/consolidate.rb
+++ lib/consolidate.rb
@@ -1,1 +1,8 @@
 require_relative "consolidate/consolidator"
+
+module Consolidate
+  def self.start
+    abort "input file not provided" unless f = ARGV.first
+    Consolidate::Consolidator.new(f).run
+  end
+end
