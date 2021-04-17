c2ef571b30aee7a84d492f99b149dfd697cc1574
Mark Delk <jethrodaniel@gmail.com>
2021-04-16 21:00:55 -0500

    fix gem cli buggos

--- lib/rubygems/commands/consolidate_command.rb
+++ lib/rubygems/commands/consolidate_command.rb
@@ -5,6 +5,22 @@ require "rubygems/command"
 class Gem::Commands::ConsolidateCommand < ::Gem::Command
   def initialize
     super "consolidate"
+
+    add_option "--footer=FOOTER" do |footer|
+      options[:footer] = footer
+    end
+
+    add_option "--header=HEADER" do |header|
+      options[:header] = header
+    end
+
+    add_option "--no-stdlib" do |bool|
+      options[:no_stdlib] = !bool
+    end
+
+    add_option "--exclude=LIBS" do |libs|
+      options[:skipped] = libs.split(',')
+    end
   end
 
   def usage
@@ -12,7 +28,7 @@ class Gem::Commands::ConsolidateCommand < ::Gem::Command
   end
 
   def description
-    Gem::Consolidate::CLI::HELP
+    Gem::Consolidate::CLI::DESC
   end
 
   def execute
